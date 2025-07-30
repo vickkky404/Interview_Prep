2b84796978fadb65fc8e5f194c44150c7a024432
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-07-30 12:10:25 +0530

    Add files via upload

---
+++ EvenElem.java
@@ -0,0 +1,13 @@
+public class EvenElem {
+    public static void main(String[] args){
+
+        int[] arr = {1,2,3,4,5,6};
+        
+        for(int i = 0 ; i < arr.length ; i++){
+            if(arr[i] % 2 == 0){
+                System.out.print(arr[i]);
+            }
+        }
+    }
+    
+}
