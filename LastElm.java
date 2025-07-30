e9eef26994330c24a9462f26ee6dc1da6a063abc
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-07-30 12:11:37 +0530

    Add files via upload

---
+++ LastElm.java
@@ -0,0 +1,11 @@
+public class LastElm {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6};
+
+        int secondLast = arr[arr.length-2];
+
+        System.out.println("The second last element is: " + secondLast);
+
+
+    }
+}
