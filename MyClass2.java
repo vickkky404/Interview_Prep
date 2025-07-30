8ab68f4d67ab4f64dfa32ed331d662ccfa89ccc6
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-07-30 12:12:19 +0530

    Add files via upload

---
+++ MyClass2.java
@@ -0,0 +1,12 @@
+public class MyClass2 {
+    public  static void main(String[] args){
+        int[] arr = {1,2,3,4,5,6};
+
+        for(int i = 0 ; i < arr.length ; i++){
+            if(i % 2 == 1){
+                System.out.print(arr[i] +" , ");
+            }
+        }
+    }
+    
+}
