a2a99347c6e5b167fd256d9c6322cc4c318da7b0
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-08-07 15:03:41 +0530

    Add files via upload

---
+++ Leng.java
@@ -0,0 +1,11 @@
+// package String Basics;
+
+
+import java.io.*;
+
+public class Leng {
+    public static void main(String[] args) {
+        String s = "gfg";
+        System.out.println(s.length());
+    }
+}
