1bb03b24452231f83c07eb0b60f7bcf28c58e549
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-08-07 13:51:09 +0530

    Add files via upload

---
+++ STRING/StrMethd.java
@@ -0,0 +1,12 @@
+// package String Basics;
+import java.io.*;
+
+public class StrMethd {
+    public static void main(String[] args) {
+        String s1 = "java";
+        s1.concat("rules");
+
+
+        System.out.println("s1 refers to " + s1);
+    }
+}
