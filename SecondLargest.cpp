f4cdc62d1a2d34775e0c231497700bfe07e4aeda
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-08-20 21:37:36 +0530

    Add files via upload

---
+++ SecondLargest.cpp
@@ -0,0 +1,36 @@
+// C++ program to find the second largest element in the array
+// using two traversals
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// function to find the second largest element in the array
+int getSecondLargest(vector<int> &arr) {
+    int n = arr.size();
+
+    int largest = -1, secondLargest = -1;
+
+    // finding the largest element
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > largest)
+            largest = arr[i];
+    }
+
+    // finding the second largest element
+    for (int i = 0; i < n; i++) {
+
+        
+        if (arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+    }
+    return secondLargest;
+}
+
+int main() {
+    vector<int> arr = {12, 35, 1, 10, 34, 1};
+    cout << getSecondLargest(arr);
+
+    return 0;
+}
