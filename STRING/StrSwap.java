c7ded8da92922b6c879e4dfae847e772ad295f32
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-08-22 16:24:49 +0530

    Add files via upload

---
+++ STRING/StrSwap.java
@@ -0,0 +1,16 @@
+// chnage the string value = "i love python " to " i love java".;
+
+public class StrSwap {
+    public static void main(String[] args){
+        String str = "I Love Python";
+
+         String Changed = str.replace("Python", "Java");
+
+        System.out.println("Old String : " + str);
+        System.out.println("New changed value: " + Changed);
+
+
+
+    }
+}
+ 
