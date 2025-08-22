d3f8b8162dc6aade16bfbcae02ee0d22de65675d
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-08-22 16:58:41 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        
+        System.out.println("Enter " + n + " numbers:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        
+        bubbleSort(arr);
+
+        
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
+
+        sc.close();
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+           
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    swapped = true;
+                }
+            }
+
+            
+            if (!swapped) break;
+        }
+    }
+}
