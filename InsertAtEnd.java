35856b0dc003173995cd7b0954c3afd4486514a0
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-08-24 19:42:15 +0530

    Add files via upload

---
+++ InsertAtEnd.java
@@ -0,0 +1,9 @@
+public class InsertAtEnd {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40};
+
+        int[] newArr = insertElement(arr , 60);
+
+        int [] afterDeletion
+    }
+}
