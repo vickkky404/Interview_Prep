17fa0121bea905a6bf6dbef50f73ff30afd41976
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-08-30 22:31:28 +0530

    Add files via upload

---
+++ STRING/ReverseString.java
@@ -0,0 +1,12 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "Java";
+        String reversed = "";
+
+        for(int i = str.length()-1 ; i >=0 ; i--){
+            reversed += str.charAt(i);
+        }
+
+        System.out.println("Reversed: " + reversed);
+    }
+}
