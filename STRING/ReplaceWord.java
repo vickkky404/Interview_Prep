ae3373daa0f274d7edd2370cbfe80e43b8cc4e16
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-08-30 22:58:24 +0530

    Add files via upload

---
+++ STRING/ReplaceWord.java
@@ -0,0 +1,10 @@
+public class ReplaceWord {
+    public static void main(String[] args){
+        String str = "I love Java";
+        String replaced = str.replace("Java" , "Python");
+
+        System.out.println(replaced);
+
+    }
+    
+}
