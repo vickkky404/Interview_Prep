fb318946763a92b8e63386d1ce728b4f69d5f72a
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-08-30 22:35:15 +0530

    Add files via upload

---
+++ STRING/EqlStr.java
@@ -0,0 +1,10 @@
+public class EqlStr {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "hello";
+
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equalsIgnoreCase(s2));
+    }
+    
+}
