337ce7d0acb5f339b30d3354e8922517a980a219
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-08-30 22:24:50 +0530

    Add files via upload

---
+++ StrUpplw.java
@@ -0,0 +1,7 @@
+public class StrUpplw {
+    public static void main(String[] args) {
+        String str = "OpenAI";
+        System.out.println("Uppercase: " + str.toUpperCase());
+        System.out.println("Lowercase: " + str.toLowerCase());
+    }
+}
