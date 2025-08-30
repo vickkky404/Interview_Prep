7a20cde7823ecbb74d5c88a2b1dc1a0b9d8f139c
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-08-30 22:47:44 +0530

    Add files via upload

---
+++ STRING/AtGivenIndex.java
@@ -0,0 +1,7 @@
+public class AtGivenIndex {
+    public static void main(String[] args){
+        String s = "This is Nalinikant.";
+
+        System.out.println("Character at index 2: " + s.charAt(2));
+    }
+}
