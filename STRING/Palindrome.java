82ee787efe17b1a9303aed44f099ab8a6c15ecd1
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-08-30 23:03:10 +0530

    Add files via upload

---
+++ STRING/Palindrome.java
@@ -0,0 +1,19 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        String str = "madam";
+        String reversed = "";
+
+        for(int i = str.length() - 1 ; i >=0 ; i++){
+            reversed += str.charAt(i);
+
+
+        }
+
+        if(str.equals(reversed)){
+            System.out.println(str + " is Palindrome");
+        }else{
+            System.out.println(str + " is not Palindrome");
+        }
+    }
+    
+}
