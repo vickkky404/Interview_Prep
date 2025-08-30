e63a1017893f095b07a97707ccd8685e76d16290
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-08-30 22:26:54 +0530

    Add files via upload

---
+++ STRING/OpenAI.java
@@ -0,0 +1,7 @@
+//to check the length
+public class OpenAI {
+    public static void main(String[] args){
+        String str = "OpenAI";
+        System.out.println("Length: " + str.length());
+    }
+}
