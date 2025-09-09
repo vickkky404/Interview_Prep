babd77a0cc7578f02b120cfb71729a7a46293a01
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-09-09 18:27:57 +0530

    Add files via upload

---
+++ STRING/CompareString.java
@@ -0,0 +1,11 @@
+public class CompareString {
+    public static void main(String[] args){
+        String str1 = "java";
+        String str2 = "cpp";
+
+        System.out.println(str1.equals(str2));
+        System.out.println(str1.compareTo(str2));
+        System.out.println(str1.equalsIgnoreCase(str2));
+
+    } 
+}
