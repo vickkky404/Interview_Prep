83282cb6a41322bf1004e2eff044966c3fac23e9
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-09-09 18:15:30 +0530

    Add files via upload

---
+++ StrConcat.java
@@ -0,0 +1,11 @@
+public class StrConcat {
+    public static void main(String[] args) {
+        String str1 = "Nalinikant";
+        String str2 = "Bastia";
+        String result = str1.concat(str2);
+        String r2 = str1 + " " + str2;
+
+        System.out.println(result);
+        System.out.println(r2);
+    }
+}
