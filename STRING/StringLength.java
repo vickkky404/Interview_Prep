f647c659eba07f58251ea53b2e174fe31c0de8d8
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-09-09 18:11:46 +0530

    Add files via upload

---
+++ STRING/StringLength.java
@@ -0,0 +1,7 @@
+public class StringLength {
+    public static void main(String[] args){
+        String str = "Hello";
+
+        System.out.println("Length: " + str.length());
+    }
+}
---
+++ STRING/StringLength2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+public class StringLength2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the String value: ");
+        String str = sc.nextLine();
+
+        System.out.println("Length of String by the user: " + str.length());
+        
+    }
+}
