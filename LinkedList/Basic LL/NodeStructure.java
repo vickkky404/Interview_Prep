d0fe9977e667da43722e1b5b69fcc2cfe310eb16
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-09-20 15:12:43 +0530

    Add files via upload

---
+++ LinkedList/Basic LL/NodeStructure.java
@@ -0,0 +1,24 @@
+public class NodeStructure{
+    public static void main(String[] args){
+
+
+        class Node{
+            int data;
+            Node next;
+
+            Node(int data){
+                this.data = data;
+                this.next = null;
+            }
+        }
+    }
+}
+
+// operations on Singly LL
+
+/*
+ * 1)create linked list & insert at end;
+ * 2)Insert at Head
+ * 3)Delete by value
+ * 4)search in lnkedlist
+ */
