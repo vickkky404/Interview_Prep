2e450576dae15b3b2ac36b73abbfeaa9cf99c3bd
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-09-21 21:42:07 +0530

    Add files via upload

---
+++ LinkedList/Basic LL/Creation.java
@@ -0,0 +1,15 @@
+
+import java.util.LinkedList;
+
+public class Creation {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+
+        System.out.println(list);
+    }
+}
