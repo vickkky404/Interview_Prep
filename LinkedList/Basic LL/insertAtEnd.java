77ba209bb3a7150f763ae5b195e4cf5a191c0bca
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-09-22 23:18:04 +0530

    Add files via upload

---
+++ LinkedList/Basic LL/insertAtEnd.java
@@ -0,0 +1,14 @@
+class Solution {
+    Node insertAtEnd(Node head, int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            return newNode;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+        return head;
+    }
+}
