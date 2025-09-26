371899b509df6a255eb14ce86df902e5fc253714
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-09-26 09:08:24 +0530

    Add files via upload

---
+++ Array Implementation/traversal.py
@@ -0,0 +1,15 @@
+# traversak in python
+def traversal(arr):
+    print("Array elements: " , end =" " )
+    for i in arr:
+        print(i, end=" ")
+
+    print()
+
+
+def main():
+        arr=[10,20,30,40,50]
+        traversal(arr)
+
+if __name__=="__main__":
+    main()
