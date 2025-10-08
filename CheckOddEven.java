bbf0d5b9e1988d2c685d8222ae92371f956169dc
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-10-08 21:53:50 +0530

    Create CheckOddEven.java

---
+++ CheckOddEven.java
@@ -0,0 +1,17 @@
+// Function to check if number is even or odd
+class Geeks {
+    //
+    static void evenOdd(int a, int b) {
+        // Your code here
+        
+        if(a % 2 == 0){
+            System.out.println(a);
+            System.out.println(b);
+            
+        }else{
+            System.out.println(b);
+            System.out.println(a);
+        }
+        
+    }
+}
