851c1d816b1dfe243cda43a31a15b7f1f998f1a4
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-10-08 22:09:11 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+// Display the most famous phrase at the of beginning any programming language, "Hello World" (without Quotes).
+
+class Solution {
+    static void printHelloWorld() {
+        // code here
+        System.out.println("Hello World"); 
+    }
+}
