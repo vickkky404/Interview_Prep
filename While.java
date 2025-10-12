41749aba1e3a376b7f47a509ad0fcf173ce61255
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-10-12 22:28:54 +0530

    Create While.java

---
+++ While.java
@@ -0,0 +1,14 @@
+//While loop is another loop like for loop but unlike for loop it only checks for one condition.
+//Here, we will use a while loop and print a number n's table in reverse order.
+
+class Solution {
+    public void calculateMultiples(int n) {
+        // code here
+
+    int i = 10;
+    while(i >= 1){
+        System.out.print(n * i + " ");
+        i--;
+     }
+    }
+}
